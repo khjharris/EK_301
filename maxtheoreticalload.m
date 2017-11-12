6caf11e17f37f6c0cd38db0a6648bd3c1c2705b0
Kenwood Harris Jr <kenwood93@gmail.com>
2017-11-11 21:11:09 -0500

    Added cost and load/cost ratio

--- maxtheoreticalload.m
+++ maxtheoreticalload.m
@@ -1,4 +1,4 @@
-function [failMember,failLoad] = maxtheoreticalload(C,x,y,T)
+function [lengths,failMember,failLoad] = maxtheoreticalload(C,x,y,T)
 %F(l) = C/l^2
 %C = 12277.78 N cm^2
 %Here l is the length of the member and C is the constant found by experimentation
@@ -24,11 +24,6 @@ for i = 1:numJoints
     end
 end
 
-%Prints out the square matrix of distances
-fprintf('Square matrix of distances: \n');
-disp(distance);
-fprintf('\n');
-
 %Produces the joints that each member is connected to
 for k=1:numMems
     for w=1:numJoints
@@ -39,40 +34,22 @@ for k=1:numMems
     end
 end
 
-%Prints out the joints each member is connected to
-fprintf('Joints connected to each member: \n');
-disp(vec);
-fprintf('\n');
-
 %Loop through vector with joints to produce vector with lengths
 for b=1:2:(numMems*2)
     lengths = [lengths distance(vec(b),vec(b+1))];
 end
-
-%Prints the lengths of the members
-fprintf('Lengths of the members: \n');
-disp(lengths);
-fprintf('\n');
 
 %Calculate the Buckling Force for each member
 for g=1:numMems
     bucklingLoad(g) = CONS/(lengths(g)*lengths(g));
 end
 
-%Prints the buckling force
-fprintf('Buckling Force for each member: \n');
-disp(bucklingLoad);
-fprintf('\n');
 
 %Calculate the SR ratio for all the members
 for u=1:numMems
     SR(u)= T(u)/bucklingLoad(u);
 end
 
-%Prints the SR ratios
-fprintf('SR ratios: \n');
-disp(SR);
-fprintf('\n');
 
 %Find the member that fails
 for m =1:numMems
@@ -86,15 +63,6 @@ end
 %Calculate the Force of failure
 failLoad = 1/SR(Failnum);
 failMember = Failnum;
-
-%Print the failmember and value
-fprintf('Failure Load: \n');
-disp(abs(failLoad));
-fprintf('\n');
-
-fprintf('Failure Member: \n');
-disp(failMember);
-fprintf('\n');
 
 end
 function r = findr(x1,x2,y1,y2)
