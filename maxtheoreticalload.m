ab1da5d0d366a71ba6628223ea759b7b8f6c49b6
Kenwood Harris Jr <kenwood93@gmail.com>
2017-11-11 19:19:50 -0500

    Print Statements
    Added Print statements for debugging

--- maxtheoreticalload.m
+++ maxtheoreticalload.m
@@ -24,6 +24,11 @@ for i = 1:numJoints
     end
 end
 
+%Prints out the square matrix of distances
+fprintf('Square matrix of distances: /n');
+disp(distance);
+fprintf('\n');
+
 %Produces the joints that each member is connected to
 for k=1:numMems
     for w=1:numJoints
@@ -34,20 +39,40 @@ for k=1:numMems
     end
 end
 
+%Prints out the joints each member is connected to
+fprint('Joints connected to each member: \n');
+disp(vec);
+fprintf('\n');
+
 %Loop through vector with joints to produce vector with lengths
 for b=1:2:(numMems*2)
     lengths(b) = distance(vec(b),vec(b+1));
 end
+
+%Prints the lengths of the members
+fprintf('Lengths of the members: \n');
+disp(lengths);
+fprintf('\n');
 
 %Calculate the Buckling Force for each member
 for g=1:numMems
     bucklingLoad(g) = CONS/(lengths(g)*lengths(g));
 end
 
-%Calculate the SR ration for all the members
+%Prints the buckling force
+fprintf('Buckling Force for each member: \n');
+disp(bucklingLoad);
+fprintf('\n');
+
+%Calculate the SR ratio for all the members
 for u=1:numMems
     SR(u)= T(u)/bucklingLoad(u);
 end
+
+%Prints the SR ratios
+fprintf('SR ratios: \n');
+disp(bucklingLoad);
+fprintf('\n');
 
 %Find the member that fails
 for m =1:numMems
@@ -57,9 +82,19 @@ for m =1:numMems
     end 
 end
 
+
 %Calculate the Force of failure
 failLoad = 1/SR(Failnum);
 failMember = Failnum;
+
+%Print the failmember and value
+fprintf('Failure Load: \n');
+disp(failLoad);
+fprintf('\n');
+
+fprintf('Failure Member: \n');
+disp(failMember);
+fprintf('\n');
 
 end
 function r = findr(x1,x2,y1,y2)
